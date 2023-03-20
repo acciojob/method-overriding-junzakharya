cac524a7500904f7ddf5ab1724da37900407bab5
Junaid Me <junaidjunni953@gmail.com>
2023-03-20 13:00:27 +0530

    Assignment methodoverride

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+        B b1 = new B();
+
+        b1.meth();
+
+        B b2 = new B();
+
+        b2.meth();
+    }
 }
